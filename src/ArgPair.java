2128fe6f894bb8919feba5e01c97188c9d7a14c8
Kargevon <nothingaboutfish@protonmail.com>
2022-01-24 20:09:02 +0200

    raw save

---
+++ src/ArgPair.java
@@ -0,0 +1,14 @@
+
+public class ArgPair {
+
+	
+	public String key;
+	public String value;
+	public ArgPair() {
+		
+	}
+	public ArgPair(String key, String value) {
+		this.key=key;
+		this.value=value;
+	}
+}
